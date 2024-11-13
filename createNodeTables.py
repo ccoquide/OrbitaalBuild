55d22217523b24d97c4a77479537dca816b287de
ccoquide <celestin.coquide@liris.cnrs.fr>
2024-11-13 12:27:30 +0100

    Add createNodeTables script with comments

---
+++ createNodeTables.py
@@ -0,0 +1,26 @@
+from collections import defaultdict as ddict
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+import datetime
+from bitunam_utils import writeNodeTable, getTimeDiff
+
+t_0=datetime.datetime.now()
+### Init pyspark session
+spark = SparkSession \
+    .builder \
+    .config("spark.driver.memory", "100g") \
+    .config("spark.executor.memory", "100g") \
+    .config("spark.local.dir","/media/ssd4/tmp")\
+    .config("spark.driver.maxResultSize","0")\
+    .getOrCreate()
+spark.sparkContext.uiWebUrl
+
+### Work Path
+PATH="/media/ssd4/celestin"
+
+### Reading cleaned data as pyspark df
+pydf=spark.read.load(f"{PATH}/Prepared_Data")
+
+### Building nodes information table with new IDs for actors (incremental and starting from 0)
+writeNodeTable(spark, pydf, PATH)
+print(f"Node table computed and written in {getTimeDiff(t_0)}")
