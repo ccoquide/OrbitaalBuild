423d6ee9d92851ff7dff46df7671d3a8456e9cf1
ccoquide <celestin.coquide@liris.cnrs.fr>
2024-11-27 11:32:10 +0100

    change with up-to-date scripts

--- finalclean.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-### PATH to temporal directory related to node table with old IDs
-PATH=sys.argv[1]
-for file in os.listdir(f"{PATH}"):
-    os.remove(f"{PATH}/{file}")
-os.removedirs(f"{PATH}")
